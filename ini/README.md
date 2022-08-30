5822962b4dcbc1666a11cadf09e7681baec66f0c
Alp <alps-examples@mailbox.org>
2022-08-30 18:31:53 +0200

    Improve ini example

--- ini/README.md
+++ ini/README.md
@@ -1,7 +1,6 @@
+The program demonstrates how to use the ini updater bash function.
 
-The program demonstrates how to modify an ini file.
-
-Starting with the ini file:
+Start with an ini file.
 
     > cat test.ini
     [leavemealone]
@@ -11,17 +10,22 @@ Starting with the ini file:
     d= 1
     #e=1
      
-We update the entries `a` and `b`  from the command line, which also shows the changes made via `diff`
 
-    > ./ini.sh test.ini a=2 b=2
+Import `ini()` function.
+
+    > . ini.sh
+
+Update the entries `a` and `b`  from the command line, which also shows the changes made via `diff`.
+
+    > ini test.ini a=2 b=2
     2,3c2,3
     < a=1
     < b=1
     ---
     > a=2
     > b=2
-    
-Afterwards, the ini file is updated
+
+Afterwards, check that the ini file is updated.
 
     > cat test.ini
     [leavemealone]
@@ -30,3 +34,8 @@ Afterwards, the ini file is updated
     c= 1
     d= 1
     #e=1
+
+Optional: Execute self-tests:
+
+    > ./ini.sh
+    Self-test PASSED.
--- ini/ini.sh
+++ ini/ini.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Update ini value in the input stream.
+# ini - Update ini value in the input stream.
 #
 # Usage:
 #
@@ -12,38 +12,68 @@
 #   - Space handling: Removes any suffix ' ' from key in stream
 #   - Simple processing: Splits key and value on first =, escape characters are not recognized
 
+ini() {
 
-# Process command line parameters
+	# Process command line parameters
 
-INI=$1
-shift
+	INI=$1
+	shift
 
-declare -A update
-for kv in "${@}"; do
-	IFS='=' read key value <<<"$kv";
-	update[$key]=$value
-done
+	declare -A update
+	for kv in "${@}"; do
+		IFS='=' read key value <<<"$kv";
+		update[$key]=$value
+	done
 
-# Create backup; setup I/O
+	# Create backup; setup I/O
 
-if [ ! "$INI" == "-" ]; then
-	cp "$INI" "$INI~"
-	exec <"$INI~"
-	exec >"$INI"
-fi
-
-# Process lines
-
-while read line; do
-	if IFS='=' read key value <<<"$line" && [ -n "$value" ]; then
-		key=${key% }
-		line="${key}=${update[${key#\#}]-$value}"
+	if [ ! "$INI" == "-" ]; then
+		cp "$INI" "$INI~"
+		exec <"$INI~"
+		exec >"$INI"
 	fi
-	echo "$line"
-done
 
-# Diff
+	# Process lines
 
-if [ ! "$INI" == "-" ]; then
-	diff -s "$INI~" "$INI" >&2
+	while read line; do
+		if IFS='=' read key value <<<"$line" && [ -n "$value" ]; then
+			key=${key% }
+			line="${key}=${update[${key#\#}]-$value}"
+		fi
+		echo "$line"
+	done
+
+	# Diff
+
+	if [ ! "$INI" == "-" ]; then
+		diff -s "$INI~" "$INI" >&2
+	fi
+}
+
+ini-test() {
+	read -d '' TEST_INP <<-EOF
+	[leavemealone]
+	a=1
+	b =1
+	c= 1
+    d = 1
+	#e=1
+	EOF
+
+	read -d '' TEST_OUT_EXP <<-EOF
+	[leavemealone]
+	a=2
+	b=2
+	c=2
+	d= 1
+	#e=1
+	EOF
+
+	TEST_OUT_ACT="$(ini - a=2 b=2 c=2 <<<"$TEST_INP")"
+	test "$TEST_OUT_ACT" == "$TEST_OUT_EXP" && echo "Self-test PASSED." || echo "Self-test failed."
+}
+
+# No harm in being a little pythonic
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    ini-test
 fi
